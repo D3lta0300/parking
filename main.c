5d68a52000e605eb18630a44e5abd1bb146448a5
Omar <omar.b682@hotmail.com>
2024-01-15 17:03:52 +0100

    paiement

--- main.c
+++ main.c
@@ -2,42 +2,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
 
-struct Voiture{
+struct Voiture {
     char immatriculation[20];
     char marque[30];
     int etage;
     int place;
+    time_t entree;  // Ajout de la variable pour enregistrer le temps dans le parking
 };
 
 typedef struct Voiture Voiture;
 
-void condenseVoiture(Voiture voiture){
+void condenseVoiture(Voiture voiture) {
     printf("%s : %s", voiture.marque, voiture.immatriculation);
 }
 
-void afficheVoiture(Voiture voiture){
+void afficheVoiture(Voiture voiture) {
     printf("La %s immatriculée %s est garée place %d, étage %d", voiture.marque, voiture.immatriculation, voiture.place, voiture.etage);
 }
 
-void initialiseParking(int nbEtage, int nbPlacesEtages, int nbVoiture, Voiture pParking[][nbPlacesEtages]){
+void initialiseParking(int nbEtage, int nbPlacesEtages, int nbVoiture, Voiture pParking[][nbPlacesEtages]) {
     int k = 0;
-    for (int i = 0;i<nbEtage;i++){
-        for (int j = 0; j<nbPlacesEtages; j++){
-            if (k<nbVoiture){
+    for (int i = 0; i < nbEtage; i++) {
+        for (int j = 0; j < nbPlacesEtages; j++) {
+            if (k < nbVoiture) {
                 Voiture initialisatrice;
-                strcpy(initialisatrice.immatriculation,"00-000-00");
+                strcpy(initialisatrice.immatriculation, "00-000-00");
                 strcpy(initialisatrice.marque, "Tesla");
-                initialisatrice.etage=i;
-                initialisatrice.place=j;
+                initialisatrice.etage = i;
+                initialisatrice.place = j;
+                initialisatrice.entree = 0;  // Initialisation du temps à l'entrée
                 pParking[i][j] = initialisatrice;
                 printf("added\n");
             } else {
                 Voiture initialisatrice;
-                strcpy(initialisatrice.immatriculation," ");
+                strcpy(initialisatrice.immatriculation, " ");
                 strcpy(initialisatrice.marque, " ");
-                initialisatrice.etage=i;
-                initialisatrice.place=j;
+                initialisatrice.etage = i;
+                initialisatrice.place = j;
+                initialisatrice.entree = 0;
                 pParking[i][j] = initialisatrice;
                 printf("added\n");
             }
@@ -46,10 +50,10 @@ void initialiseParking(int nbEtage, int nbPlacesEtages, int nbVoiture, Voiture p
     }
 }
 
-void afficheParking(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]){
+void afficheParking(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]) {
     printf("\nAffichage en cours\n");
-    for (int i = 0;i<nbEtage;i++){
-        for (int j = 0; j<nbPlacesEtages; j++){
+    for (int i = 0; i < nbEtage; i++) {
+        for (int j = 0; j < nbPlacesEtages; j++) {
             condenseVoiture(parking[i][j]);
             printf(" | ");
         }
@@ -57,93 +61,90 @@ void afficheParking(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesE
     }
 }
 
-
-void sortDuParking(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]){
-    printf("\n\nÀ quel étage êtes vous garé ? ");
+void sortDuParking(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]) {
+    printf("\n\nÀ quel étage êtes-vous garé ? ");
     int etage;
-    scanf("%d",&etage);
+    scanf("%d", &etage);
 
-    printf("\nÀ quelle place êtes vous garé ? ");
+    printf("\nÀ quelle place êtes-vous garé ? ");
     int place;
-    scanf("%d",&place);
+    scanf("%d", &place);
 
-    if(strncmp(parking[etage][place].immatriculation, " ",1)==0){
-        printf("\nIl n'y a pas de voiture ici. Êtes vous un fantôme ?");
+    if (strncmp(parking[etage][place].immatriculation, " ", 1) == 0) {
+        printf("\nIl n'y a pas de voiture ici. Êtes-vous un fantôme ?");
     } else {
+        time_t maintenant;
+        time(&maintenant);
+
+        double duree = (double) difftime(maintenant, parking[etage][place].entree);
+        double paye = duree * (1./60.);
+        printf("La voiture est restée dans le parking pendant %.2f secondes et doit payer %2f €.\n", duree, paye);
+
         Voiture initialisatrice;
-        strcpy(initialisatrice.immatriculation," ");
+        strcpy(initialisatrice.immatriculation, " ");
         strcpy(initialisatrice.marque, " ");
-        initialisatrice.etage=etage;
-        initialisatrice.place=place;
+        initialisatrice.etage = etage;
+        initialisatrice.place = place;
+        initialisatrice.entree = 0;
         parking[etage][place] = initialisatrice;
-        printf("deleted\n");
+        printf("place libérée\n");
     }
 }
 
-
-void ajouteVoiture(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]){
-
+void ajouteVoiture(int nbEtage, int nbPlacesEtages, Voiture parking[][nbPlacesEtages]) {
     bool isPlaceOk = false;
     int etage;
     int place;
 
-    while(!isPlaceOk){
-        printf("\n\nÀ quelle étage souhaitez vous vous garer ?");
-        scanf("%d",&place);
+    while (!isPlaceOk) {
+        printf("\n\nÀ quel étage souhaitez-vous vous garer ? ");
+        scanf("%d", &etage);
 
-    printf("Vous allez vous garer à la place %d, etage %d",place,etage);
+        printf("\nÀ quelle place souhaitez-vous vous garer ? ");
+        scanf("%d", &place);
 
-        printf("\nÀ quelle place souhaitez vous vous garer ?");
-        scanf("%d",&place);
-
-        if(strncmp(parking[etage][place].immatriculation," ",1)){
-            printf("\nCette place est déjà prise, vous n'allez quand même pas écraser cette pauvre voiture ?\nReccomence.\n");
+        if (strncmp(parking[etage][place].immatriculation, " ", 1)) {
+            printf("\nCette place est déjà prise, vous n'allez quand même pas écraser cette pauvre voiture ?\nRecommence.\n");
         } else {
             printf("\nLa voie est libre, fonce Alfons !");
             isPlaceOk = true;
         }
     }
 
-    
-
-    printf("\nQuel est la marque de votre voiture ?");
+    printf("\nQuel est la marque de votre voiture ? ");
     char marque[50];
-    scanf("%s",marque);
+    scanf("%s", marque);
 
-    printf("\nQuelle est votre plaque d'immatriculation ?");
+    printf("\nQuelle est votre plaque d'immatriculation ? ");
     char immatriculation[50];
-    scanf("%s",immatriculation);
+    scanf("%s", immatriculation);
+
+    time_t maintenant;
+    time(&maintenant);
 
     Voiture nouvelle;
-    strcpy(nouvelle.immatriculation,immatriculation);
+    strcpy(nouvelle.immatriculation, immatriculation);
     strcpy(nouvelle.marque, marque);
-    strcpy(nouvelle.immatriculation,immatriculation);
-    strcpy(nouvelle.marque, marque);
-    nouvelle.etage=etage;
-    nouvelle.place=place;
-
-    afficheVoiture(nouvelle);
-
+    nouvelle.etage = etage;
+    nouvelle.place = place;
+    nouvelle.entree = maintenant;  // Enregistrement du temps d'entrée
 
     afficheVoiture(nouvelle);
 
     parking[etage][place] = nouvelle;
 
-    afficheParking(nbEtage,nbPlacesEtages,parking);
-
-    afficheParking(nbEtage,nbPlacesEtages,parking);
+    afficheParking(nbEtage, nbPlacesEtages, parking);
 }
 
-
-int main(){
+int main() {
     printf("Bienvenue dans mon super programme de PARKING !!!\n");
     int nbVoiture = 4;
     int nbEtage = 2;
     int nbPlacesEtages = 3;
     Voiture parking[nbEtage][nbPlacesEtages];
-    while (1==1) {
-        printf("\nMon super parking à %d places !!! Malheureusement pour vous, %d sont déjà occupées.", nbEtage*nbPlacesEtages, nbVoiture);
-        printf("\nQue souhaitez vous faire ? 'Entrer', 'Sortir', 'Initialiser' ou 'Afficher' ? \n");
+    while (1 == 1) {
+        printf("\nMon super parking à %d places !!! Malheureusement pour vous, %d sont déjà occupées.", nbEtage * nbPlacesEtages, nbVoiture);
+        printf("\nQue souhaitez-vous faire ? 'Entrer', 'Sortir', 'Initialiser' ou 'Afficher' ? \n");
 
         char reponse[40];
         scanf("%s", reponse);
@@ -153,29 +154,28 @@ int main(){
         const char* initialiser = "Initialiser";
         const char* afficher = "Afficher";
 
-
-        if (strncmp(entrer,reponse,3)==0){
+        if (strncmp(entrer, reponse, 3) == 0) {
             if (nbVoiture >= nbEtage * nbPlacesEtages) {
-                printf("Désolé, mais mon super parking est plein. Revenez plus tard.\n");}
-            else {
+                printf("Désolé, mais mon super parking est plein. Revenez plus tard.\n");
+            } else {
                 ajouteVoiture(nbEtage, nbPlacesEtages, parking);
                 printf("\nOh non, une voiture s'ajoute dans le parking...\n\n");
-                nbVoiture++;}
-        } else if (strncmp(sortir,reponse,3)==0){
-            sortDuParking(nbEtage,nbPlacesEtages,parking);
+                nbVoiture++;
+            }
+        } else if (strncmp(sortir, reponse, 3) == 0) {
+            sortDuParking(nbEtage, nbPlacesEtages, parking);
             printf("Ouiii, je m'alège!!!\n\n");
             nbVoiture--;
-        } else if (strncmp(initialiser,reponse,3)==0){
+        } else if (strncmp(initialiser, reponse, 3) == 0) {
             printf("Initialisation du parking ...");
-            initialiseParking(nbEtage,nbPlacesEtages, nbVoiture, parking);
+            initialiseParking(nbEtage, nbPlacesEtages, nbVoiture, parking);
             printf(" terminée ! \n\n");
             afficheVoiture(parking[0][1]);
-        } else if (strncmp(afficher,reponse,3)==0){
+        } else if (strncmp(afficher, reponse, 3) == 0) {
             afficheParking(nbEtage, nbPlacesEtages, parking);
         } else {
             printf("C'est au CP qu'on apprend à écrire, recommence.\n\n");
         }
-
     }
     return 0;
-}// String compare pour verifier si la place qu'on demande est deja prise ou non
+}
